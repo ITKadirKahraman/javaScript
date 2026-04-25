26c325bd53f37df1850f5fa9bfb4383822287b0c
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-04-25 22:49:45 +0200

    Create json.js

---
+++ objects/json.js
@@ -0,0 +1,15 @@
+/*
+****** JSON 
+Ist ein textbasiertes Datendocument, das primär zum Austausch von Daten zwischen Servern und Webanwendungen verwendet wird.
+
+Das ist die JSON Notation
+Valide Datentypen: 
+string, number, object, array, boolean and null
+*/
+
+let validJsonObject = {
+    "name": "Kadir", // Gänsefüßchen unterschied
+    isAGoodGuy: true
+};
+
+console.log(validJsonObject)
